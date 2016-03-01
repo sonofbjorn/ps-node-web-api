1c06c66ae2aa621365eac0395d15b5f1098f1701
sonofbjorn <labjornson@gmail.com>
2016-03-01 16:40:38 -0600

    added async package and added async call to pet.js

--- routes/pet.js
+++ routes/pet.js
@@ -1,17 +1,51 @@
 var r = require('request').defaults({
-  // send data in json format
-  json: true
+    // send data in json format
+    json: true
 });
 
+var async = require('async');
+
 module.exports = function(app) {
-  // Read
-  app.get('/pets', function (req, res) {
-    r({uri: 'http://localhost:3001/dog'}, function(error, response, body) {
-      if (!error && response.statusCode === 200) {
-        res.json(body);
-      } else {
-        res.send(response.statusCode);
-      }
+    // Read
+
+    app.get('/pets', function(req, res) {
+        async.parallel({
+                cat: function(callback) {
+                    r({
+                        uri: 'http://localhost:3000/cat'
+                    }, function(error, response, body) {
+                        if (error) {
+                          callback({service: 'cat', error: error});
+                          return;
+                        }
+                        if (!error && response.statusCode === 200) {
+                            callback(null, body);
+                        } else {
+                            res.send(response.statusCode);
+                        }
+                    });
+                },
+                dog: function(callback) {
+                    r({
+                        uri: 'http://localhost:3001/dog'
+                    }, function(error, response, body) {
+                        if (error) {
+                          callback({service: 'dog', error: error});
+                          return;
+                        }
+                        if (!error && response.statusCode === 200) {
+                            callback(null, body);
+                        } else {
+                            res.send(response.statusCode);
+                        }
+                    });
+                }
+            },
+            function(error, results) {
+              res.json({
+                error: error,
+                results: results
+              })
+            });
     });
-  });
 }
